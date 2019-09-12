600cc097773bf3f07ad195c2887fffa5efe24738
CGomez258 <47128712+CGomez258@users.noreply.github.com>
2019-09-12 15:30:34 -0600

    Update test_Tarea2.py

--- test_Tarea2.py
+++ test_Tarea2.py
@@ -1,11 +1,11 @@
 import Tarea2
 
-def test_funcion():
+def test_exito():
     prueba1 = Tarea2.funcion(2,1) #probar func para el caso de exito --> sirve
     assert prueba1 == (1,3,2)
-def test_f():
+def test_error():
     prueba2 = Tarea2.funcion(1,2) # error debido que el primer valor es menor que es segundo
     assert prueba2 == -25
-def test_f2(): #verifica el error de tipos y busca un resultado igual a -15
+def test_error2(): #verifica el error de tipos y busca un resultado igual a -15
     prueba3 = Tarea2.funcion("a",2)
     assert prueba3 == -15
